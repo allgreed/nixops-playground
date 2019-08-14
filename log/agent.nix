5a6d12db5656ad858230192e6f6edaffcce20036
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-08-14 21:37:07 +0200

    Add TODOs

--- log/agent.nix
+++ log/agent.nix
@@ -12,13 +12,26 @@ in
       vimHugeX
   ];
 
-  # TODO: compile a TODO from squire's ansibles
+  # TODO: ops accounts (from seperate file)
+  # TODO: add ssh banner
+  # TODO: secure ssh
+
+  # TODO: shared FS across nodes - full replication
+
+  # TODO: ensure machines won't go to sleep after closing lid
+  # TODO: add management scripts from Squire
+
+  # TODO: copy apps configs (how about stically through etc? :D)
+  # TODO: copy job descriptions
+  # TODO: tune them so that they work
+  # TODO: move ephemeral data
 
   virtualisation.docker.enable = true;
 
   users.mutableUsers = false;
 
   # TODO: Move it into a real service without --dev
+  # TODO: how about containers and using system stuff? ;D
   systemd.services.consul-dev = {
       description = "Consul client and server";
 
