4d1dcab5c501563b704a7e90a27c7ffe9ae7ea61
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-10-18 01:06:17 +0200

    Add operator accounts

--- logic/agent.nix
+++ logic/agent.nix
@@ -6,6 +6,7 @@ in
 {
   imports = [
     ./nomad.nix
+    ./operators.nix
   ];
 
   environment.systemPackages = with pkgs; [
@@ -15,7 +16,6 @@ in
 
   # TODO: add direnv + default.nix
 
-  # TODO: ops accounts (from seperate file)
   # TODO: move gluster mounts into nixos
   # TODO: ensure machines won't go to sleep after closing lid
   # TODO: bring cluster-zwei online
@@ -23,20 +23,20 @@ in
   # TODO: extract Consul
   # TODO: add ssh banner
   # TODO: secure ssh (will it break nixops?)
+  # TODO: backups!
 
   # TODO: copy apps configs (how about stically through etc? :D) - Consul
   # TODO: copy job descriptions
   # TODO: tune them so that they work
-  # TODO: backups!
 
   # TODO: add management scripts from Squire vs. make sure they're not needed
   # TODO: move persistent data
 
-  # TODO: setup distributed DB
-  # TODO: do better security XD
   # TODO: change generic DNS to OpenNic
+  # TODO: do better security XD
     # add explicit users and minimum privilages everywhere
     # firewall - is doing firewall worth it in this case?
+    # go through "security" options on nixos
 
   virtualisation.docker.enable = true;
   networking.firewall.enable = false;
@@ -60,15 +60,7 @@ in
   services.glusterfs.enable = true;
 
   users.mutableUsers = false;
-  # TODO: Parametrize with stuff from operators.nix
-  users.users.allgreed = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "docker" ];
-      openssh.authorizedKeys.keys = [
-            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDWnwdY4pSLVyuLwMARJ6tQtnxcrQS4dx5RM+HiVBj4HSGWSSGNpbFlwcUx4bDuSII6hofuoPy83OvtKs/n1+SWv9UDue72slXamh2XbTXDtA2IG2XiiaiXSUUbJX5/ejKE+90/OK87ccGpDFgJVAD53EMV6NUJXWbDKwVrAnVEzoCPqGLYGDPs389pzM1OYHFzbuWAh5Wlrv/05j4T8b5fB+QsX87Z8FT0tDwPjUYzsd6ugL7Vf5EU8HkLAH+9m128OMGcOuv5bTVUVbR7CI7c8bmw2+nw7AjgX7oexQFC+fevSKYVRbusZ88jbz5sUhCC58d3mdfmYxME3z/sD37Cr0HTUBOEWS4eP0BqF0w+tTTl3bsXCUhs35cMIoUY8SRuij3zqsGNDqWhVuVFwI5uYJOXEtBfQuI/79inJhLHi/SwnXu1FXJ0q7kRureMR9EnrZ8LEMNZ9rrFwCdhJXIlHzu9vpbMlpbSAkiHmfiigcCZFxyBr/GRRj6srTGxkv63fsOYOVfvTSzUa4cpqxEcD+0Yhyr7mf/OfpdwTaR/r8SPvP3CJUme2pviXP7FxcVYKhHMAJTLQ2xMwEt6yyqs/RR9/lYdQFyCwM5oBZQqZxHJMSqdXp+ZUEFa5orKWvaxBisLQy2tIEqE77h22er3zK0VFK/ETE/3Cxdz3HtlOQ== allgreed@terminator"
-      ];
-      initialHashedPassword = "$6$sEk83.F2VbsYW$iILuEeRZZE5aIh87UIze4R7g82JGavVkm3yURcI38Zka5M/djEClUEr0.PWklwdea0UrGKrNAx3B.BKh435Uu0"; # please change the password via local.nix ASAP
-  };
+  security.sudo.wheelNeedsPassword = false; # at least until I figure out how to securely set passwords across multiple machines
 
   # TODO: Move it into a real service without --dev
   # TODO: how about containers and using system stuff? ;D
